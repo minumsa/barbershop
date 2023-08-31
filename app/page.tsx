b6ad1ce860716644aca738befd89db170acd57a6
유일 <83910706+minumsa@users.noreply.github.com>
2023-08-31 16:00:26 +0900

    상단 탭 만들기

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,54 @@
-export default function Home() {
+"use client";
+
+import { useState } from "react";
+
+export default function Page() {
+  const [isFilterActive, setIsFilterActive] = useState<boolean>(false);
+
+  const handleFilter = () => {
+    setIsFilterActive(!isFilterActive);
+  };
+
   return (
     <div className="container">
+      <div
+        className="filter-content"
+        style={isFilterActive ? { position: "fixed" } : { display: "none" }}
+      ></div>
+      <div
+        className="close"
+        style={isFilterActive ? { position: "absolute" } : { display: "none" }}
+        onClick={() => {
+          handleFilter();
+        }}
+      >
+        ×
+      </div>
       <div className="nav-container">
         <div className="title">
-          <div>마이바버</div>
+          <div>마이바버샵</div>
         </div>
         <div className="search-container">
           <div className="search">
+            <div className="search-button">
+              <div style={{ paddingRight: "10px" }}>🔍</div>
+            </div>
             <input className="search-input" placeholder="지역을 입력해주세요" />
             <div className="search-button">
-              <div>검색</div>
+              <div style={{ paddingLeft: "10px" }}>검색</div>
             </div>
           </div>
         </div>
         <div className="category">
           <div className="category-content">지도 ▾</div>
-          <div className="category-content">36,000원 이하 ▾</div>
+          <div
+            className="category-content"
+            onClick={() => {
+              handleFilter();
+            }}
+          >
+            필터 ▾
+          </div>
         </div>
       </div>
       <div className="content-container"></div>
