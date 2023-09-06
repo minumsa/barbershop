4895763cceb95e37100b44ab8a455c59b94d3dc8
유일 <83910706+minumsa@users.noreply.github.com>
2023-09-06 16:07:23 +0900

    바버샵 정보창 띄우기

--- app/Map.tsx
+++ app/Map.tsx
@@ -11,12 +11,11 @@ export const Map = () => {
 
     // 로케이션표시 Google Maps에서 원하는 장소 찾은 후 주변검색을 누르면 좌표를 찾을 수 있음
     // FIXME: 나중에 기본 장소 변경하기 - 현재 위치? 고민해보기!
-    const location = new naver.maps.LatLng(37.5663, 126.9779);
-    // const harfBarberShop = new naver.maps.LatLng(37.56571603771177, 126.99485276474563);
+    const harfbarbershop = new naver.maps.LatLng(37.56571603771177, 126.99485276474563);
 
     // 네이버 지도 옵션 선택
     const mapOptions = {
-      center: location,
+      center: harfbarbershop,
       zoom: 16,
       zoomControl: true,
       zoomControlOptions: {
@@ -25,15 +24,49 @@ export const Map = () => {
     };
     const map = new naver.maps.Map(mapElement.current, mapOptions);
 
+    var harfbarbershopString = [
+      '<div class="is_inner">',
+      "   <div>하프바버샵</div>",
+      "<div class='overlay_detail'>",
+      "   <div class='overlay_detail_title'>주소</div>",
+      "   <div>서울 중구 을지로18길 15 2층 205호</div>",
+      "</div>",
+      "<div class='overlay_detail'>",
+      "   <div class='overlay_detail_title'>운영시간</div>",
+      "   <div>월~토 11:00~20:00</div>",
+      "</div>",
+      "<div class='overlay_detail'>",
+      "   <div class='overlay_detail_title'>휴무일</div>",
+      "   <div>매주 일요일</div>",
+      "</div>",
+      "<div class='overlay_detail'>",
+      "   <div class='overlay_detail_title'>연락처</div>",
+      "   <div>0507-1329-2972</div>",
+      "</div>",
+      "</div>",
+    ].join("");
+
     // 지도상에 핀 표시 할 부분
-    new naver.maps.Marker({
-      position: location,
+    var harfbarbershopMarker = new naver.maps.Marker({
+      position: harfbarbershop,
       map: map,
     });
     // new naver.maps.Marker({
-    //   position: harfBarberShop,
+    //   position: harfbarbershop,
     //   map: map,
     // });
+
+    var infowindow = new naver.maps.InfoWindow({
+      content: harfbarbershopString,
+    });
+
+    naver.maps.Event.addListener(harfbarbershopMarker, "click", function (e) {
+      if (infowindow.getMap()) {
+        infowindow.close();
+      } else {
+        infowindow.open(map, harfbarbershopMarker);
+      }
+    });
   }, []);
 
   return <div ref={mapElement} style={{ width: "100%", height: "100%" }} />;
--- app/SideBar.tsx
+++ app/SideBar.tsx
@@ -1,3 +1,5 @@
+import { barbershopArray } from "./lib/data";
+
 export const SideBar = () => {
   return (
     <div className="side-bar">
@@ -12,20 +14,25 @@ export const SideBar = () => {
           <option value="name-reverse">상호명 내림차순</option>
         </select>
       </div>
-      <div className="barbershop-list">
-        <div className="barbershop-image"></div>
-        <div className="barbershop-info">
-          <div className="barbershop-info-header">
-            <div className="barbershop-name">하프바버샵</div>
-            <div className="barbershop-hashtag">#정통클래식 #을지로힙플</div>
+      {barbershopArray.map((data, index) => {
+        return (
+          <div className="barbershop-list" key={index}>
+            <div className="barbershop-image"></div>
+            <div className="barbershop-info">
+              <div className="barbershop-info-header">
+                <div className="barbershop-name">{data.name}</div>
+                <div className="barbershop-hashtag">
+                  {data.hashtag.map((tag, index) => {
+                    return <span key={index} style={{ paddingRight: "5px" }}>{`#${tag}`}</span>;
+                  })}
+                </div>
+              </div>
+              <div className="barbershop-location">{data.location}</div>
+              <div className="barbershop-introduce">{data.notice}</div>
+            </div>
           </div>
-          <div className="barbershop-location">서울 중구 을지로18길 15 2층 205호</div>
-          <div className="barbershop-introduce">
-            을지로 3가와 4가 사이 인쇄 골목에 위치한 1인 바버샵입니다. 한 분 한 분 최선을
-            다하겠습니다.
-          </div>
-        </div>
-      </div>
+        );
+      })}
     </div>
   );
 };
