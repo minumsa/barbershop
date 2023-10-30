95a316d6c3f49e7bd51b12453afa386be5218d44
minumsa <icycyi@naver.com>
2023-10-30 17:32:07 +0900

    필터 윈도우 초기화 버튼 만들기

--- app/components/LoadingItems.tsx
+++ app/components/LoadingItems.tsx
@@ -4,39 +4,19 @@ export const LoadingItems = () => {
   const LoadingItem = () => {
     return (
       <div className={styles["list-container"]}>
-        <div
-          className={styles["barbershop-image-container"]}
-          style={{ border: "none", backgroundColor: "#dbdbdb" }}
-        ></div>
+        <div className={styles["barbershop-image-container"]}></div>
         <div className={styles["list-information"]}>
-          <div
-            className={styles["list-name"]}
-            style={{
-              backgroundColor: "#e6e6e6",
-              width: "85px",
-              height: "20px",
-              borderRadius: "5px",
-              marginBottom: "10px",
-            }}
-          >
+          <div className={`${styles["list-name"]} ${styles["list-name-loading"]}`}>
             <div className={styles["list-name-text"]}></div>
           </div>
-          <div
-            className={styles["list-location"]}
-            style={{
-              backgroundColor: "#e6e6e6",
-              width: "230px",
-              height: "20px",
-              borderRadius: "5px",
-            }}
-          ></div>
+          <div className={`${styles["list-location"]} ${styles["list-location-loading"]}`}></div>
         </div>
       </div>
     );
   };
 
   return (
-    <div>
+    <>
       <LoadingItem />
       <LoadingItem />
       <LoadingItem />
@@ -44,6 +24,6 @@ export const LoadingItems = () => {
       <LoadingItem />
       <LoadingItem />
       <LoadingItem />
-    </div>
+    </>
   );
 };
