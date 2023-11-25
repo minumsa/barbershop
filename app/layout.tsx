2fd9b6e3e2160cf0cf2db74ef0994790703c16f1
minumsa <icycyi@naver.com>
2023-11-25 21:14:20 +0900

    input 클릭 시 자동확대 방지

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html>
       <head>
+        <meta
+          name="viewport"
+          content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width"
+        />
         {/* eslint-disable-next-line @next/next/no-before-interactive-script-outside-document */}
         <Script
           src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NAVER_MAP_CLIENT_ID}`}
